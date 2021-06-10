62a07b8087da33d2a4155bcfe34c907abc426e0d
Noken29 <42448187+Noken29@users.noreply.github.com>
2021-06-10 19:58:33 +0300

    Deleted TextResultTransformer from Context

--- src/main/java/com/task/wordcounter/model/util/TextResultTransformer.java
+++ src/main/java/com/task/wordcounter/model/util/TextResultTransformer.java
@@ -3,9 +3,7 @@ package com.task.wordcounter.model.util;
 import com.task.wordcounter.entity.TextResult;
 import com.task.wordcounter.model.TextResultDto;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
-@Component
 @NoArgsConstructor
 public class TextResultTransformer {
     public static TextResultDto transform(TextResult textResult){
